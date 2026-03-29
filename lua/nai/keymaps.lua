2cbacfe102fca885970e0bf2bfa68fd9f4bd0497
Naimul Hasan <nhasan@splunk.com>
2026-03-29 12:50:53 -0400

    add custom keybinds for windo navigation

--- lua/nai/keymaps.lua
+++ lua/nai/keymaps.lua
@@ -7,4 +7,10 @@ keymap.set("n", "<C-f>", "<cmd>Telescope live_grep<CR>", { desc = "Search in fil
 -- File explorer
 keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 
+-- Window navigation (using Ctrl+h/j/k/l - vim standard)
+keymap.set("n", "<C-j>", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+keymap.set("n", "<C-k>", "<C-w>j", { desc = "Move to bottom window" })
+keymap.set("n", "<C-i>", "<C-w>k", { desc = "Move to top window" })
+
 print("Keymaps loaded!")
