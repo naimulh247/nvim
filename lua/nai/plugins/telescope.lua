12a802b75c6cb980ae34e5a7f832fac0c053b454
Naimul Hasan <nhasan@splunk.com>
2026-03-29 11:03:38 -0400

    add telescope plugin

---
+++ lua/nai/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      },
+    })
+
+    telescope.load_extension("fzf")
+    print("Telescope loaded!")
+  end,
+}
